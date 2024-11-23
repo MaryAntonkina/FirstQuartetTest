bc7f0ad6c6053c9715a6e5e36f373921ee0050a0
MaryAntonkina <gunina-91@mail.ru>
2024-11-23 18:31:32 +0700

    mvp

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+
+        string[] array = { "раз", "два", "три", "четыре", "пять" };
+        int taskLength = 3;
+        string[] resultArray = new string[0];
+        int resultLength = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i].Length <= taskLength)
+            {
+                Array.Resize(ref resultArray, resultLength + 1);
+
+                resultArray[resultLength] = array[i];
+                resultLength++;
+            }
+        }
+
+        Console.WriteLine("Результирующий массив:");
+        Console.WriteLine(string.Join(", ", resultArray));
+    }
+}
